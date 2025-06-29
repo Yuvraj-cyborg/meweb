bc952c794090c7d2535d26e59ef6ba40aadc048a
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-06-30 00:36:36 +0530

    new-work and now section update

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -4,12 +4,13 @@ export default function Now() {
         <h1 className="text-3xl mb-6">Now</h1>
         
         <div className="space-y-4 text-sm leading-6">
-          <h2 className="text-xl">May 2025</h2>
+          <h2 className="text-xl">June 2025</h2>
           
           <ul className="list-disc pl-5 space-y-2">
-            <li>Completing my second year in Computer Science at Silicon University</li>
-            <li>Working on freelance web development projects</li>
-            <li>Learning advanced Rust and web3</li>
+            <li>Entered my third year in Computer Science at Silicon University</li>
+            <li>Working on freelance system level projects</li>
+            <li>Working on an Operating System in Rust</li>
+            <li>Learning Web3</li>
         </ul>
         </div>
       </main>
