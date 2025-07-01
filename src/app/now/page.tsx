c01ccf8a01cffb6af7c1d72c0770a6b03a59a036
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-07-02 00:06:59 +0530

    date-updated

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -4,7 +4,7 @@ export default function Now() {
         <h1 className="text-3xl mb-6">Now</h1>
         
         <div className="space-y-4 text-sm leading-6">
-          <h2 className="text-xl">June 2025</h2>
+          <h2 className="text-xl">July 2025</h2>
           
           <ul className="list-disc pl-5 space-y-2">
             <li>Entered my third year in Computer Science at Silicon University</li>
