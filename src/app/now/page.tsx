8e641233e26fd11c64adc9ebede361a973624e14
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-07-31 23:44:47 +0530

    feat: now section updated

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -4,13 +4,13 @@ export default function Now() {
         <h1 className="text-3xl mb-6">Now</h1>
         
         <div className="space-y-4 text-sm leading-6">
-          <h2 className="text-xl">July 2025</h2>
+          <h2 className="text-xl">August 2025</h2>
           
           <ul className="list-disc pl-5 space-y-2">
             <li>Entered my third year in Computer Science at Silicon University</li>
-            <li>Working on freelance system level projects</li>
-            <li>Working on an Operating System in Rust</li>
-            <li>Learning Web3</li>
+            <li>Working at Dognosis tech as a software developer</li>
+            <li>Fellow at Superteam Solana Fellowship</li>
+            <li>Fellow at School of Solana by Ackee</li>
         </ul>
         </div>
       </main>
