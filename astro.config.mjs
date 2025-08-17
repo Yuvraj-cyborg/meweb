acb3564c57c85ad6efb76178bbef8afb6205f723
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-08-18 00:47:50 +0530

    feat: package change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "static",
   site: "https://yuvraj-portfolio.vercel.app",
   integrations: [react()],
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
