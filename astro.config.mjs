b243b1053ad9bab072b310c4ba931407cd3399cb
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-08-19 00:25:48 +0530

    feat: blink fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "static",
   site: "https://yuvraj-dev.me",
   integrations: [react()],
-  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
